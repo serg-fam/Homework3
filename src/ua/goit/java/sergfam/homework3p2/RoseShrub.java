2fd9f83fb6cd1dadd673a162d6b2e629d17b505c
serg-fam <sergifam@gmail.com>
2016-03-10 22:50:08 +0200

    ver5

--- src/ua/goit/java/sergfam/homework3p2/RoseShrub.java
+++ src/ua/goit/java/sergfam/homework3p2/RoseShrub.java
@@ -5,7 +5,7 @@ import java.util.List;
 /**
  * Created by Serg-fam on 09.03.2016 on 14:56.
  */
-public class RoseShrub extends Flower {
+public class RoseShrub  {
 
 
     private List<Rose> roses;
