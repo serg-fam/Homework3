af1d6a44954d965fdfef591b8c4476be4085dcdd
serg-fam <sergifam@gmail.com>
2016-03-10 23:07:22 +0200

    Update File.java

--- src/ua/goit/java/sergfam/homework3p1/File.java
+++ src/ua/goit/java/sergfam/homework3p1/File.java
@@ -6,34 +6,5 @@ import java.util.List;
  * Created by Serg-fam on 09.03.2016 on 14:11.
  */
 public class File {
-    private List<Image> images;
-    private List<Audio> audios;
-    private List<Text> texts;
-
-
-    public List<Image> getImages() {
-        return images;
-    }
-
-    public void setImages(List<Image> images) {
-        this.images = images;
-    }
-
-    public List<Audio> getAudios() {
-        return audios;
-    }
-
-    public void setAudios(List<Audio> audios) {
-        this.audios = audios;
-    }
-
-    public List<Text> getTexts() {
-        return texts;
-    }
-
-    public void setTexts(List<Text> texts) {
-        this.texts = texts;
-    }
-
 
 }
